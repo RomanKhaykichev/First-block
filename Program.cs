c87996cf2996ec770ddd93cbbaa32d38dc36511e
Roman_Khaykichev <mr.romanx@mail.ru>
2023-03-24 19:56:13 +0300

    Добавлено описание и вывод заданного массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+
+int Print(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+string PrintString(string message)
+{
+    Console.Write(message);
+    return Convert.ToString(Console.ReadLine());
+}
+
+string[] FillArray(int lenght)
+{
+    string[] array = new string[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = PrintString($"Введите строку массива {i+1}: ");
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write($"Mas -> [{String.Join(", ", array)}]");
+}
+
+
+
+int lenght = Print("Введите длину массива: ");
+string[] getArray = FillArray(lenght);
+PrintArray(getArray);
